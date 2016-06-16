52d9170c7cd4c7163c09b335b6f7e162bc1a7730
Noble Ackerson <noble.ackerson@gmail.com>
2016-06-16 13:42:06 -0400

    Update config.js

--- app/config.js
+++ app/config.js
@@ -2,13 +2,13 @@
   'use strict';
   angular.module('lynxfit')
     //define firebase authenticate
-    .constant('FBURL', 'https://lynxplus.firebaseio.com')
+    .constant('FBURL', 'https://<your-fb>.firebaseio.com') // you should probably update to firebase2
     .constant('SIMPLE_LOGIN_PROVIDERS', ['google'])//'password', 'anonymous', 'facebook', 'google', 'twitter', 'github'
     .constant('loginRedirectPath', '/signin')
 
     //define google cloud storage settings
     .constant('GOOGLE_SETTINGS', {
-      clientId: '559798132783-hm3gc0lj0rups45i35m7mp4s88u7321o.apps.googleusercontent.com',
+      clientId: '[your id].apps.googleusercontent.com',
       scope: 'https://www.googleapis.com/auth/userinfo.email',
       apiKey: '[YOURKEY]',
       bucket: 'lynxcontent',
